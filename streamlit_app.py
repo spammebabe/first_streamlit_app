2b7fa6359a0b7f34b93586c4fb4472bca1ba11f3
spammebabe <150343824+spammebabe@users.noreply.github.com>
2023-11-09 15:14:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner!')
 
-#streamlit.header('Breakfast Menu') 
+streamlit.header('Breakfast Menu') 
 #streamlit.text('Omega 3 & Blueberry Oatmeal') 
 #streamlit.text('Kale, Spinach 8 Rocket Smoothie') 
 #streamlit.text('Hard-80iIed Free-Range Egg') 
