a2e3471504f662880a33f956876928c35ac1b498
spammebabe <150343824+spammebabe@users.noreply.github.com>
2023-11-09 15:08:12 +0100

    Update streamlit_app.py
    Neue Menüpunkte

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 
 import streamlit
 
-streamlit.title('Hello World!')
+streamlit.title('My parents new healthy dinner!')
+Streamlit. header( 'Breakfast Menu' ) 
+streamlit. text( 'Omega 3 & Blueberry Oatmeal' ) 
+streamlit. text( 'Kale, Spinach 8 Rocket Smoothie') 
+streænit.text( 'Hard-80iIed Free-Range Egg ' ) 
